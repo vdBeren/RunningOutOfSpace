9a3baec7be2a8965f92f6c1aa5fc0b5fdae8421d
vdBeren <vdias.savariego@gmail.com>
2018-08-12 01:34:29 -0300

    Initial commit

---
+++ Assets/_Scripts/CatController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cat {
+	public class CatController : MonoBehaviour {
+		//THIS IS THE CAT
+
+		public GameObject paws;
+
+		bool holdingItem = false;
+		GameObject itemHovered;
+		GameObject itemHeld;
+
+
+		// Use this for initialization
+		void Start () {
+			Cursor.visible = false;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			FollowMouse ();
+			Action ();
+		}
+
+
+		void FollowMouse(){
+			// THE CAT FOLLOWS THE MOUSE HEHE
+			var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			this.transform.position = new Vector3 (mousePos.x, mousePos.y, -1);
+
+		}
+
+		void Action(){
+			if (Input.GetMouseButtonDown(0)) {
+				
+				if (!holdingItem)
+					PickItem ();
+				else
+					ReleaseItem ();
+			}
+		}
+
+		void PickItem (){
+
+			if (itemHovered == null)
+				return;
+
+			holdingItem = true;
+			itemHeld = itemHovered;
+
+			Vector3 pawsPostion = paws.transform.position;
+
+			itemHeld.transform.position = new Vector3(pawsPostion.x, pawsPostion.y, pawsPostion.z - 1);
+			itemHeld.transform.SetParent (paws.transform);
+		}
+
+		void ReleaseItem (){
+			
+			holdingItem = false;
+			itemHeld.transform.SetParent (null);
+
+			var grid = Cat.GameManager.instance.itemGrid;
+			var cellPos = grid.WorldToCell(itemHeld.transform.position);
+
+			itemHeld.transform.position = grid.GetCellCenterWorld (cellPos);
+
+			itemHeld = null;
+			itemHovered = null;
+		}
+
+		void OnTriggerStay2D (Collider2D col){
+					
+			if (col.gameObject.tag == "Item") {
+				itemHovered = col.gameObject;
+			}
+		}
+
+		void OnTriggerExit2D(Collider2D col){
+			
+			if (col.gameObject.tag == "Item") {
+				itemHovered = null;
+			}
+		}
+	}
+}
---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cat{
+	public class GameManager : MonoBehaviour {
+
+		public Grid itemGrid;
+
+		public static GameManager instance = null;
+
+		// Use this for initialization
+		void Start () {
+
+			if (instance == null)
+				instance = this;
+
+			DontDestroyOnLoad (this.gameObject);
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
